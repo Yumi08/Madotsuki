de366a36a6affaa02f6490ecf32fbb7154297662
Yumi Ta <soraishi08@gmail.com>
2020-02-02 15:54:58 -0500

    Setup cogs

--- Bot.py
+++ Bot.py
@@ -9,6 +9,18 @@ logging.basicConfig(level=logging.INFO)
 
 client = commands.Bot(command_prefix = "$")
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f"Cogs.{extension}")
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f"Cogs.{extension}")
+
+for filename in os.listdir("./Cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f"Cogs.{filename[:-3]}")
+
 @client.event
 async def on_ready():
     print("Bot ready.")
