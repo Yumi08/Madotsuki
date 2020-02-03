741c5ae07b1b8932d56cdc2f0b6c9c77e96a6098
Yumi Ta <soraishi08@gmail.com>
2020-02-02 20:04:34 -0500

    Setup logging

--- Bot.py
+++ Bot.py
@@ -2,13 +2,17 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import logging
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 client = commands.Bot(command_prefix = "$")
 
 @client.event
 async def on_ready():
     print("Bot ready.")
+    await client.change_presence(activity=discord.Game("I love you."))
 
 client.run(os.getenv("TOKEN"))
+
